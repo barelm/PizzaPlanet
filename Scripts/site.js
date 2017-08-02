ac852951a05cebdb1e2bc983a42b1d945290b1ab
gavelight <ornatan777@gmail.com>
2017-08-02 20:16:48 +0300

    Adding home and about html files and working on routing

---
+++ Scripts/site.js
@@ -0,0 +1,46 @@
+﻿// Write your Javascript code.
+var canvas = document.getElementById("canvas");
+var context = canvas.getContext("2d");
+
+function drawClock() {
+    // clear the background
+    context.fillStyle = 'lightgray';
+    context.fillRect(0, 0, canvas.width, canvas.height);
+
+    // Get the current time
+    var now = new Date(),
+        h = now.getHours(),
+        m = now.getMinutes(),
+        s = now.getSeconds();
+
+    // Make values like '5' into '05'
+    h = addLeadingZeroWhenNecessary(h);
+    m = addLeadingZeroWhenNecessary(m);
+    s = addLeadingZeroWhenNecessary(s);
+
+    // Assemble the text
+    var clockText = h + ':' + m + ':' + s,
+        x = 110,
+        y = 35;
+
+    // This green color was picked
+    // using http://jscolor.com/
+    context.fillStyle = '#1A75AB';
+
+    // Draw the text
+    //context.font = '50pt Arial';
+    context.font = '20pt Arial';
+    context.strokeStyle = 'black';
+    context.fillText(clockText, x, y);
+    context.strokeText(clockText, x, y);
+}
+
+function addLeadingZeroWhenNecessary(s) {
+    return (s < 10 ? '0' : '') + s;
+}
+
+// Draw the clock right away
+drawClock();
+
+// Then draw the clock every subsequent second
+setInterval(drawClock, 1000);
--- app/app.js
+++ app/app.js
@@ -1,3 +1,23 @@
 /**
  * Created by evronor on 02/08/2017.
  */
+
+var app = angular.module('pizzaApp', ['ngRoute']);
+
+//This configures the routes and associates each route with a view and a controller
+app.config(function ($routeProvider) {
+    $routeProvider
+        // Defines a route to 'home'
+        .when('/home',
+            {
+                controller: '',
+                templateUrl: '../../app/partials/home.html'
+            })
+        // Defines a root to 'about'
+        .when('/about',
+            {
+                controller: '',
+                templateUrl: '../../app/partials/about.html'
+            })
+        .otherwise({ redirectTo: '/home' });
+});

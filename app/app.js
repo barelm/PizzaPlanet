79fca5f88428bcc91146293877467358606416b0
gavelight <ornatan777@gmail.com>
2017-08-04 01:49:28 +0300

    Fixing navigations and also showing pictures / videos and changing title dynamically

--- app/app.js
+++ app/app.js
@@ -5,19 +5,31 @@
 var app = angular.module('pizzaApp', ['ngRoute']);
 
 //This configures the routes and associates each route with a view and a controller
-app.config(function ($routeProvider) {
+app.config(function ($routeProvider, $locationProvider) {
     $routeProvider
         // Defines a route to 'home'
         .when('/home',
             {
+                title: 'פיצה פלאנט',
                 controller: '',
                 templateUrl: 'app/partials/home.html'
             })
         // Defines a root to 'about'
         .when('/about',
             {
+                title: 'אודות',
                 controller: '',
                 templateUrl: 'app/partials/about.html'
             })
         .otherwise({ redirectTo: '/home' });
+
+    // Remove unnecessary prefix
+    $locationProvider.hashPrefix('');
 });
+
+// Changes title according to selected navigation
+app.run(['$rootScope', function($rootScope) {
+    $rootScope.$on('$routeChangeSuccess', function (event, current, previous) {
+        $rootScope.title = current.$$route.title;
+    });
+}]);

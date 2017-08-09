a22e0538fdb4cb7ed479424f93f8688ffdf9fe19
natanor <ornatan777@gmail.com>
2017-08-09 17:25:44 +0300

    more fixes

--- app/controllers/BranchController.js
+++ app/controllers/BranchController.js
@@ -118,3 +118,75 @@ app.controller('BranchEditController', function ($scope, $routeParams ,$location
         $location.path('/branch')
     }
 });
+
+app.controller('BranchMapController', function ($scope, $routeParams, BranchService) {
+
+    //I like to have an init() for controllers that need to perform some initialization. Keeps things in
+    //one place...not required though especially in the simple example below
+    init();
+
+    function init() {
+        $scope.branches = BranchService.getBranches();
+        console.log($scope.branches);
+
+        var script = document.createElement('script');
+        script.type = 'text/javascript';
+        script.src = "http://maps.google.com/maps/api/js?key=AIzaSyCij6Yc19nHcMJNb63ioB4yYgy3N6CH5aU";
+        script.onload = function(){
+
+            // Set the margin bottom of the blue wrap
+            $('#blue').css("margin-bottom", "0px");
+
+            // Set Israel as the center of the map
+            var Israel = new google.maps.LatLng(31.046051, 34.851612);
+
+            // These are options that set initial zoom level, where the map is centered globally to start, and the type of map to show
+            var mapOptions = {
+                zoom: 8,
+                center: Israel,
+                mapTypeId: google.maps.MapTypeId.G_NORMAL_MAP
+            };
+
+            // This makes the div with id "map_canvas" a google map
+            var map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+
+            // Create the geocoder object
+            var geocoder = new google.maps.Geocoder();
+
+            // Get the branches data from the controller, in JSON format
+            var branchesData = $scope.branches;
+
+            // Using the JQuery "each" selector to iterate through the JSON list and drop marker pins
+            $.each(branchesData, function (i, item) {
+
+                // Initiate a request to the Google geocoding service to get the location of the branch by the address
+                geocoder.geocode({'address': item.address}, function(results, status) {
+
+                    // Set marker in the relevant location on the map
+                    var marker = new google.maps.Marker({
+                        'position': results[0].geometry.location,
+                        'map': map,
+                        'title': item.name
+                    });
+
+                    // Make the marker-pin blue
+                    marker.setIcon('http://maps.google.com/mapfiles/ms/icons/blue-dot.png')
+
+                    // Put in some information about each JSON object - branch's name. address and phone number
+                    var infowindow = new google.maps.InfoWindow({
+                        content: "<div class='infoDiv'><h2><u><center>" + item.name + "</h2></u></center>" + "<h3><center>" + item.address + "<br>" + item.phoneNumber + "</h3></center>" + "</div></div>"
+                    });
+
+                    // Finally hook up an "OnClick" listener to the map so it pops up out info-window when the marker-pin is clicked
+                    google.maps.event.addListener(marker, 'click', function () {
+                        infowindow.open(map, marker);
+                    });
+                });
+            })
+
+            console.log('Done');
+        }
+
+        document.body.appendChild(script).then;
+    }
+});

efdbc08046e7f53bace94443474f47faf539d6ae
Oron Evron <oronevron@gmail.com>
2017-08-09 18:59:46 +0300

    Use cors in server

--- server/server.js
+++ server/server.js
@@ -3,9 +3,12 @@
  */
 
 var express = require("express");
+var cors = require("cors");
+var routes = require("./routes");
 var app = express();
 
-app.use(require('./routes'));
+app.use(cors());
+app.use(routes);
 
 app.listen(3000, function() {
     console.log('Server running at port %s!', this.address().port)

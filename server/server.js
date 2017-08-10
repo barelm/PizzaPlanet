904545e9745cae30a92ddc41f2afe6d7804c3f43
Oron <oronevron@gmail.com>
2017-08-10 20:22:27 +0300

    Add support for parsing of application/json type post data (with body-parser)

--- server/server.js
+++ server/server.js
@@ -5,7 +5,11 @@
 var express = require("express");
 var cors = require("cors");
 var routes = require("./routes");
+var bodyParser = require('body-parser');
 var app = express();
+
+// Add support for parsing of application/json type post data
+app.use(bodyParser.json());
 
 app.use(cors());
 app.use(routes);

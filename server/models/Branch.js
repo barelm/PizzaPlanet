ed35481a9e7388afc8727dccfd12a13306b39776
Oron <oronevron@gmail.com>
2017-08-11 15:12:50 +0300

    Add mongoose schema and model of Branch

---
+++ server/models/Branch.js
@@ -0,0 +1,20 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+// create a schema
+var branchSchema = new Schema({
+    id: {type: Number, required: true, unique: true},
+    Name: {type: String, required: true},
+    Region: {type: String, required: true},
+    City: {type: String, required: true},
+    IsKosher: Boolean,
+    Address: {type: String, required: true},
+    IsDisabledAccessible: Boolean
+});
+
+// the schema is useless so far
+// we need to create a model using it
+var Branch = mongoose.model('Branch', branchSchema);
+
+// make this available to our users in our Node applications
+module.exports = Branch;

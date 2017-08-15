5d38d042fe092b490a51e6d5e308c77191ef6a02
Oron Evron <oronevron@gmail.com>
2017-08-15 15:28:39 +0300

    Add enum to sex field in employee model

--- server/models/Employee.js
+++ server/models/Employee.js
@@ -5,7 +5,7 @@ var Schema = mongoose.Schema;
 var employeeSchema = new Schema({
     id: {type: Number, required: true, unique: true},
     Name: {type: String, required: true},
-    Sex: {type: String, required: true},
+    Sex: {type: String, required: true, enum: ["זכר", "נקבה"]},
     Role: {type: String, required: true},
     Wage: {type: Number, required: true, min: 0},
     City: {type: String, required: true},
